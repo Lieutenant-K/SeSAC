25040010652bdcbf790213852a7a3bc1e6f6612b
Lieutenant-K <kts4047@gmail.com>
2022-07-08 09:22:08 +0900

    Initial Commit

---
+++ SesacDelivery/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  SesacDelivery
+//
+//  Created by 김윤수 on 2022/07/05.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bannerImageView.image = UIImage(named: "banner0\(Int.random(in: 1...3))")
+        bannerImageView.layer.cornerRadius = 25
+        bannerImageView.layer.borderColor = UIColor.yellow.cgColor
+        bannerImageView.layer.borderWidth = 5
+        
+        
+        label.text = "베달의 민족"
+        label.backgroundColor = .blue
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 30)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func resultButtonAction(_ sender: UIButton) {
+        
+        bannerImageView.image = UIImage(named: "banner0\(Int.random(in: 1...3))")
+        
+    }
+}
---
+++ SesacDelivery/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SesacDelivery
+//
+//  Created by 김윤수 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button: UIButton?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
