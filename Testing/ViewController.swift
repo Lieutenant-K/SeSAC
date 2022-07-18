f25713bbf6f5d33c72e4d5626141511d0a092f6a
Lieutenant-K <kts4047@gmail.com>
2022-07-18 22:02:01 +0900

    subtree test

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        prin("서브트리 테스트")
         // Do any additional setup after loading the view.
     }
 
