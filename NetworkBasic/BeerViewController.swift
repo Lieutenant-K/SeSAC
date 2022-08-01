1862cbd2a9db9febc49f1b9a14695a1cf136fcd1
Lieutenant-K <kts4047@gmail.com>
2022-08-02 01:28:26 +0900

    add beer view controller

---
+++ NetworkBasic/BeerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BeerViewController.swift
+//  NetworkBasic
+//
+//  Created by 김윤수 on 2022/08/02.
+//
+
+import UIKit
+
+class BeerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
