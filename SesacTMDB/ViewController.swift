8b10bd245c6944f1c967962ab02e18d957de8c1d
Lieutenant-K <kts4047@gmail.com>
2022-08-03 09:40:05 +0900

    Initial Commit

---
+++ SesacTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SesacTMDB
+//
+//  Created by 김윤수 on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
